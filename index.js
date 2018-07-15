c755548954625c16ec566b3b4e1041e39fd6c394
SunilDy <sunilsahudy@yahoo.com>
2018-07-15 16:33:55 +0530

    added mlab connection string

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose')
 
+// Connecting to Mlab
+const DB_USER = process.env.DB_USER
+const DB_PASS = process.env.DB_PASS
+mongoose.connect(`mongodb://${DB_USER}:${DB_PASS}@ds239071.mlab.com:39071/my-api`);
+mongoose.connection.once('open', () => {
+    console.log(`Connected to Mlab`)
+})
+
+// PORT config
 const PORT = process.env.PORT || 5000;
 
 app.get('/', (req,res) => {
